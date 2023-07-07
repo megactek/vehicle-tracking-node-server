4e384bb9dcd80363de8f24240772934d7a77a05c
Adedamola Emmanuel <mgcrons@gmail.com>
2023-07-07 06:53:35 +0100

    adjusted server

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const morgan = require("morgan");
-app.use(express.json());
 
 const trucksRoute = require("./routes/trucks");
 const userRoute = require("./routes/users");
+
+const morgan = require("morgan");
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(morgan("common"));
+app.use("/api/trucks", trucksRoute);
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+  res.write("<h1>get your lame ass off here</h1>");
+  res.end();
+});
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,8 +34,23 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-app.use(morgan("common"));
-app.use("/api/trucks", trucksRoute);
-app.use("/api/users", userRoute);
+io.on("connection", (socket) => {
+  console.log("connected to socket");
 
-app.listen(PORT, () => console.log("Server is running on port " + PORT));
+  socket.on("updateCoord", ({ truckId, lat, long }) => {
+    console.log("sent coordinates ", lat, long, " to ", truckId);
+    io.emit("newCoord", { truckId, lat, long });
+    io.emit("refreshCoord", {});
+  });
+
+  socket.on("deleteCoord", () => {
+    console.log("refreshed");
+    io.emit("refreshCoord", {});
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+http.listen(PORT, () => console.log("Server is running on port " + PORT));
