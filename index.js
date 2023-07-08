5e81bd15304c079d1fa9e785be6a4d8192793dd4
Adedamola Emmanuel <mgcrons@gmail.com>
2023-07-08 21:22:11 +0100

    deployment update

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const trucksRoute = require("./routes/trucks");
 const userRoute = require("./routes/users");
 
 const morgan = require("morgan");
+const cors = require("cors");
 
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(morgan("common"));
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content- Type, Accept");
+  next();
+});
+
 app.use("/api/trucks", trucksRoute);
 app.use("/api/users", userRoute);
 
@@ -20,8 +35,10 @@ app.get("/", (req, res) => {
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: ["http://localhost:3000", "https://vehicle-tracking-react-app.vercel.app/", "http://127.0.0.1:3000"],
+    origin: true,
+    credentials: true,
   },
+  allowEIO3: true,
 });
 const mongoose = require("mongoose");
 
